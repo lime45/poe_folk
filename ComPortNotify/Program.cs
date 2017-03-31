e70e15da52f391783ff2458a02ab44c1d98e57e3
Chris Browning <christopher.browning@synapsewireless.com>
2017-03-30 21:10:35 -0500

    rename foo.exe to something more descriptive

--- ComPortNotify/Program.cs
+++ ComPortNotify/Program.cs
@@ -12,10 +12,12 @@ namespace ComPortNotify
         static void Main()
         {
 			NAppUpdate.Framework.UpdateManager.Instance.UpdateSource = new NAppUpdate.Framework.Sources.SimpleWebSource("https://www.dropbox.com/s/4czsu1rljsj7lec/feed.xml?dl=1");
-            NAppUpdate.Framework.UpdateManager.Instance.ReinstateIfRestarted();
+			NAppUpdate.Framework.UpdateManager.Instance.Config.UpdateExecutableName = "COM Port Notifier Updater.exe";
+			NAppUpdate.Framework.UpdateManager.Instance.Config.UpdateProcessName = "COM Port Notifier Updater";
+			NAppUpdate.Framework.UpdateManager.Instance.ReinstateIfRestarted();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MyApplicationContext());
-        }
+		}
     }
 }
