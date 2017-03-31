e81bb1eff23f24f44362194798646e75358d4921
Chris Browning <christopher.browning@synapsewireless.com>
2017-03-30 20:30:19 -0500

    Revert "fix updater"
    This reverts commit 23bf47c8a5ca79a5ca83054d1fd7e0c1e614db31.

--- ComPortNotify/MyApplicationContext.cs
+++ ComPortNotify/MyApplicationContext.cs
@@ -169,12 +169,13 @@ namespace ComPortNotify
         {
             // Get a local pointer to the UpdateManager instance
             UpdateManager updManager = UpdateManager.Instance;
-			var nConfig = new NauConfigurations{
-				// change these names to something besides 'foo'
-				UpdateExecutableName = "poe_update.exe",
-				UpdateProcessName = "poe_update"
-			};
-			updManager.CheckForUpdates();
+            NauConfigurations nConfig = null;
+
+            // change these names to something besides 'foo'
+            nConfig.UpdateExecutableName = "poe_update.exe";
+            nConfig.UpdateProcessName = "poe_update";
+
+            updManager.CheckForUpdates();
             if (updManager.UpdatesAvailable != 0)
             {
                 DialogResult dr = MessageBox.Show(
